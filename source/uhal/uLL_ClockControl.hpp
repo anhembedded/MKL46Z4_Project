49722ad608dc3a9b012c2ad754c8aba1b8137932
Anh.Embedded <Anh.Embedded@gmail.com>
2022-10-07 10:34:56 +0700

    uLL_ClockControl add file

---
+++ source/uhal/uLL_ClockControl.hpp
@@ -0,0 +1,38 @@
+/* ********************************************************************************************************* */
+/*                                                                                                           */
+/*                                                              :::::::::: ::::::::   :::::::: :::::::::::   */
+/*   uLL_ClockControl.hpp                                      :+:       :+:    :+: :+:    :+:    :+:        */
+/*                                                            +:+       +:+        +:+           +:+         */
+/*   By: Nathan Embedded <Anh.Embedded@gmail.com>            +#++:++#  +#++:++#++ :#:           +#+          */
+/*                                                          +#+              +#+ +#+   +#+#    +#+           */
+/*   Created: 2022/10/07 10:29:20 by Nathan Embed          #+#       #+#    #+# #+#    #+#    #+#            */
+/*   Updated: 2022/10/07 10:29:20 by Nathan Embed         ########## ########   ######## ###########         */
+/*                                                                                                           */
+/* ********************************************************************************************************* */
+
+#ifndef MKL46Z4_PROJECT_SOURCE_UHAL_ULL_CLOCKCONTROL_HPP
+#define MKL46Z4_PROJECT_SOURCE_UHAL_ULL_CLOCKCONTROL_HPP
+
+/*--------------------------------------------------------------------
+  For including C's header files
+--------------------------------------------------------------------*/
+extern "C"
+{
+#include "CMSIS/cmsis_compiler.h"
+#include "board/board.h"
+}
+/*--------------------------------------------------------------------
+  For including Cpp's header files
+--------------------------------------------------------------------*/
+#include "bitManipulation.hpp"
+#include <cstdint>
+
+
+namespace iLL_clockControl
+{
+    
+
+
+} //namespace iLL_clockControl
+
+#endif // MKL46Z4_PROJECT_SOURCE_UHAL_ULL_CLOCKCONTROL_HPP
