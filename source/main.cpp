81503b3263f9e615e6daebd0da4608f8677493da
Anh.Embedded <Anh.Embedded@gmail.com>
2022-10-12 23:47:11 +0700

    OK nha

---
+++ source/main.cpp
@@ -0,0 +1,62 @@
+/* ********************************************************************************************************* */
+/*                                                                                                           */
+/*                                                              :::::::::: ::::::::   :::::::: :::::::::::   */
+/*   MKL46Z4_Learning.cpp                                      :+:       :+:    :+: :+:    :+:    :+:        */
+/*                                                            +:+       +:+        +:+           +:+         */
+/*   By: Nathan Embedded <Anh.Embedded@gmail.com>            +#++:++#  +#++:++#++ :#:           +#+          */
+/*                                                          +#+              +#+ +#+   +#+#    +#+           */
+/*   Created: 2022/10/07 18:35:08 by Nathan Embed          #+#       #+#    #+# #+#    #+#    #+#            */
+/*   Updated: 2022/10/07 18:35:08 by Nathan Embed         ########## ########   ######## ###########         */
+/*                                                                                                           */
+/* ********************************************************************************************************* */
+
+extern "C"
+{
+#include "board.h"
+#include "MKL46Z4.h"
+}
+
+#include "uhal/uLL_BoaedLed/uLL_BoardLED.hpp"
+#include "uhal/uLL_ClockControl/uLL_ClockControl.hpp"
+#include "uhal/uLL_BoardButton/uLL_BoardButton.hpp"
+volatile uint32_t msTicks = 123; /* Variable to store millisecond ticks */
+
+extern "C"
+{
+void SysTick_Handler(void)
+{
+    /* SysTick interrupt Handler. */
+    msTicks++; /* See startup file startup_LPC17xx.s for SysTick vector */
+    NVIC_ClearPendingIRQ(SysTick_IRQn);
+}
+}
+
+uint32_t watchVar = 0;
+
+int main(void)
+{
+    clockControl::systemClock::enableClkPortD();
+    clockControl::systemClock::enableClkPortE();
+    OnBoardLED::init();
+    OnBoardButton::init();
+
+    SysTick_Config(SystemCoreClock / 1000);
+
+    while (true)
+    {
+        if (OnBoardButton::SW1::isPress())
+        {
+            OnBoardLED::ledRed::off();
+            watchVar = 1;
+        }
+
+        if (!OnBoardButton::SW1::isPress())
+        {
+
+            OnBoardLED::ledRed::on();
+            watchVar = 0;
+        }
+    }
+
+    return (0);
+}
